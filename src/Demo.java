d1ff72bfad06b05f2b59114d24b797315fba4273
penkolebamovski <penkopr13@gmail.com>
2015-11-12 11:35:57 +0200

    added all file

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+/**
+ * Created by clouway on 15-11-12.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        File opr = new File();
+
+        System.out.println("The sum is:" + opr.sum(3,4));
+    }
+}
---
+++ src/File.java
@@ -0,0 +1,11 @@
+/**
+ * Created by clouway on 15-11-12.
+ */
+public class File {
+    public int sum(int a,int b)
+    {
+        int c;
+        c=a+b;
+        return c;
+    }
+}
